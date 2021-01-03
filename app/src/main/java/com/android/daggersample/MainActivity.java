e283071b72c0fceee82d28325188b6d9c3fe7677
Rd Rundla <rdrundla@gmail.com>
2021-01-03 11:37:46 +0530

    Component, Provision method and Constructor Injection.

--- app/src/main/java/com/android/daggersample/MainActivity.java
+++ app/src/main/java/com/android/daggersample/MainActivity.java
@@ -2,14 +2,23 @@ package com.android.daggersample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private Car car;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: ");
         setContentView(R.layout.activity_main);
+
+        CarComponent component = DaggerCarComponent.create();
+        car = component.getCar();
+        car.drive();
     }
 }
